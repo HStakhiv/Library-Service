fff4cb2bfd0d70d2d633905e1f1797f50a7c49e3
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-11-29 19:39:28 +0200

    Add swagger docs

--- library_service/urls.py
+++ library_service/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("book.urls", namespace="books")),
+    path('api/docs/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
