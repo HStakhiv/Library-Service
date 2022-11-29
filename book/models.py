a3f28c0f6b12b6baa8ea1def5710cb55e7bd4ecf
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-29 15:41:45 +0200

    Add Book List View

--- book/models.py
+++ book/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -16,12 +16,19 @@ class Book(models.Model):
         choices=COVER_CHOICES,
         default=HARD,
     )
-    inventory = models.IntegerField(validators=MinValueValidator(1))
+    inventory = models.IntegerField(unique=True, validators=[
+        MinValueValidator(0),
+        MaxValueValidator(5000),
+    ])
     daily_fee = models.DecimalField(
-        max_digits=3,
-        decimal_places=2
+        max_digits=5,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.00),
+            MaxValueValidator(999.99),
+        ],
     )
 
     def __str__(self):
         """String for representing the Model object."""
-        return self.title
+        return f"{self.title}-{self.author}: {self.daily_fee}$/day"
--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book.models import Book
+from book.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
