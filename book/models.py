8fe0fc280f8c0fc315bb9b58496ade212300d14a
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-29 14:51:22 +0200

    Add book model and register it in admin panel

--- book/models.py
+++ book/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    HARD = "HARD"
+    SOFT = "SOFT"
+    COVER_CHOICES = [
+        (HARD, "Hard"),
+        (SOFT, "Soft"),
+    ]
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=4,
+        choices=COVER_CHOICES,
+        default=HARD,
+    )
+    inventory = models.IntegerField(validators=MinValueValidator(1))
+    daily_fee = models.DecimalField(
+        max_digits=3,
+        decimal_places=2
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
