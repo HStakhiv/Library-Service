6856dfe24099046e2cbf148394352e4a70d7fc9d
HStakhiv <gsstahiv@gmail.com>
2022-11-29 14:54:08 +0200

    Add new apps into settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "book",
+    "user",
+    "borrowing",
+    "payment",
 ]
 
 MIDDLEWARE = [
