2707181d09aed7dc071fdc9f9984f88ea8c7e8ab
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-11-29 15:54:42 +0200

    Implement search by book title, author and inventory number

--- book/views.py
+++ book/views.py
@@ -8,3 +8,22 @@ from book.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_queryset(self):
+        """Retrieve the movies with filters"""
+        title = self.request.query_params.get("title")
+        author = self.request.query_params.get("author")
+        inventory = self.request.query_params.get("inventory")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if author:
+            queryset = queryset.filter(author__icontains=author)
+
+        if inventory:
+            queryset = queryset.filter(inventory=inventory)
+
+        return queryset.distinct()
