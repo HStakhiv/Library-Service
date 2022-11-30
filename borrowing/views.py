1e41f995be9442a571ddaf5860c92083820424e9
Nick Semenyk <96727778+graandda@users.noreply.github.com>
2022-12-01 01:24:28 +0200

    fix return logic (#24)
    * fix return logic
    
    * fix return list borrowing

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -8,6 +10,7 @@ from borrowing.serializers import (
     BorrowingAdminSerializer,
     BorrowingAdminDetailSerializer,
     BorrowingCreateSerializer,
+    BookReturnBorrowingSerializer,
 )
 
 
@@ -41,7 +44,24 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         if self.action == "create":
             return BorrowingCreateSerializer
 
+        if self.action == "return_book":
+            return BookReturnBorrowingSerializer
+
         return BorrowingSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(url_path="return",
+            detail=True,
+            methods=["post"])
+    def return_book(self, request, pk):
+
+        borrowing = Borrowing.objects.get(id=pk)
+        serializer = self.get_serializer(borrowing, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
