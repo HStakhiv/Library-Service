6a1fdddf6282cc5d0247d160a95fd16b6e20f963
Nick Semenyk <96727778+graandda@users.noreply.github.com>
2022-11-30 17:10:41 +0200

    add borrowing vilidations (#15)
    * add borrowing vilidations
    
    * fix codestyle

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,14 +1,28 @@
+from django.conf import settings
 from django.db import models
 
 from book.models import Book
+from user.models import User
 
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
-    book = models.ManyToManyField(Book, related_name="borrowed_books")
-    # user = models.ForeignKey(User, related_name="borrowed_user")
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(
+        Book, related_name="borrowing_books", on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="borrowing_user",
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
         ordering = ["borrow_date"]
+
+    @staticmethod
+    def decrease_book_inventory(pk):
+        book = Book.objects.get(pk=pk)
+        book.inventory -= 1
+        book.save()
