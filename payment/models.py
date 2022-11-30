3348acecf041c3f302b976b320e9f57aeb3ce744
Khrystyna Skrypko <111378604+matechris@users.noreply.github.com>
2022-11-30 22:53:35 +0200

    Create list & detail Payments endpoint (#21)

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,52 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+from user.models import User
+
+
+class Payment(models.Model):
+    PENDING = "PENDING"
+    PAID = "PAID"
+    STATUS_CHOICES = [
+        (PENDING, "Pending"),
+        (PAID, "Paid"),
+    ]
+    PAYMENT = "PAYMENT"
+    FINE = "FINE"
+    TYPE_CHOICES = [
+        (PAYMENT, "Payment"),
+        (FINE, "Fine")
+    ]
+    status = models.CharField(
+        max_length=7,
+        choices=STATUS_CHOICES
+    )
+    type = models.CharField(
+        max_length=7,
+        choices=TYPE_CHOICES,
+        default=PAYMENT
+    )
+    borrowing_id = models.ForeignKey(
+        to=Borrowing,
+        on_delete=models.CASCADE,
+        related_name="payments"
+    )
+    money_to_pay = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0.00),
+            MaxValueValidator(999.99),
+        ],
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="payments"
+    )
+
+    # next fields will be added when session is implemented
+    # session_url = models.URLField()
+    # session_id = models.IntegerField(auto_created=True)
