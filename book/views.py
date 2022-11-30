234aa011405d6393c80a6f4c935d289a9b723026
Skrypka Dmytro <108864360+skrypka-dmytro@users.noreply.github.com>
2022-11-30 22:12:04 +0200

    Test book api (#12)
    * Create test for book api
    
    * Add permission_classes for BookViewSet
    
    * Add test for search book

--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from book.models import Book
 from book.permissions import IsAdminOrIfNotReadOnly
