0518589c7bfc16eec4439f6557a9df7c05a625d1
Vadym Shkarbul <111114742+VadymShkarbul@users.noreply.github.com>
2022-11-30 11:09:23 +0200

    Book service (#11)
    * Remove unnessesarly search
    
    * Add permisions to book

--- book/views.py
+++ book/views.py
@@ -3,18 +3,19 @@ from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets
 
 from book.models import Book
+from book.permissions import IsAdminOrIfNotReadOnly
 from book.serializers import BookSerializer, BookCreateSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrIfNotReadOnly, )
 
     def get_queryset(self):
         """Retrieve the movies with filters"""
         title = self.request.query_params.get("title")
         author = self.request.query_params.get("author")
-        inventory = self.request.query_params.get("inventory")
 
         queryset = self.queryset
 
@@ -23,9 +24,6 @@ class BookViewSet(viewsets.ModelViewSet):
 
         if author:
             queryset = queryset.filter(author__icontains=author)
-
-        if inventory:
-            queryset = queryset.filter(inventory=inventory)
 
         return queryset.distinct()
 
